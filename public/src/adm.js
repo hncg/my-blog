2ce983f981d824883a6a4642ba31eee6b2351d93
hncg <574878649@qq.com>
2016-03-02 21:03:34 +0800

    1.增加后台，2.下拉刷新

---
+++ public/src/adm.js
@@ -0,0 +1,59 @@
+var Title = React.createClass({
+    componentDidMount: function() {
+    },
+    render: function() {
+    return (
+        <div id='title' >
+            <input contentEditable="true" placeholder='标题' />
+        </div>
+    );
+    }
+
+});
+
+var Admin = React.createClass({
+    handleSubmit: function(e){
+        e.preventDefault();
+        content = this.state.ue.getContent();
+        title = $('#title input').val();
+        author = 'cg';
+        data = JSON.stringify({"title": title, "content": content, "author": author});
+        $.ajax({
+            url: this.props.url,
+            dataType: 'json',
+            type: 'post',
+            cache: false,
+            data: data,
+            success: function(data) {
+              alert('发布成功');
+            }.bind(this),
+            error: function(xhr, status, err) {
+              console.error(this.props.url, status, err.toString());
+            }.bind(this)
+        });
+    },
+    getInitialState: function() {
+        return {data: [],
+                ue: UE.getEditor('container'),
+            };
+    },
+    componentDidMount: function() {
+    },
+    render: function() {
+    return (
+        <div className='blog-edit'>
+            <Title />
+            <script id="container" name="content" type="text/plain"></script>
+            <div className='btn'>
+                <button className="btn-apply" type="button" onClick={this.handleSubmit}>提交</button>
+            </div>
+        </div>
+    );
+    }
+});
+
+ReactDOM.render(
+  <Admin url='http://blog.cg.com/api/v1/user/1/article' pollInterval={60000}/>,
+  document.getElementById('admin')
+);
+
--- public/src/cg.js
+++ public/src/cg.js
@@ -1,6 +1,14 @@
 function rawMarkup(txt) {
-  var rawMarkup = marked(txt, {sanitize: true});
-  return { __html: rawMarkup };
+  rawMark = marked(txt, {sanitize: false});
+  return { __html: rawMark };
+};
+
+String.prototype.format=function()
+{
+  if(arguments.length==0) return this;
+  for(var s=this, i=0; i<arguments.length; i++)
+    s=s.replace(new RegExp("\\{"+i+"\\}","g"), arguments[i]);
+  return s;
 };
 
 var Comment = React.createClass({
@@ -10,9 +18,9 @@ var Comment = React.createClass({
         <span className="commentUser">
           <a>{this.props.data.user_niker}</a>
         </span>
-        <div dangerouslySetInnerHTML=rawMarkup({this.props.data.content})>
-          {}
-        </div>
+        <span className='commentContent'>
+          {this.props.data.content}
+        </span>
       </div>
     );
   }
@@ -22,7 +30,7 @@ var CommentList = React.createClass({
   render: function() {
     var commentNodes = this.props.data.map(function(comment){
         return (
-            <Comment key={comment.id} data={comment} />
+            <Comment key={comment.id} data={comment}></Comment>
         );
     });
     return (
@@ -55,7 +63,7 @@ var CommentSubmit = React.createClass({
 var CommentInput = React.createClass({
   render: function() {
     return (
-      <div className="commentInput w-input-text" contentEditable="true" maxLength="1024" ref="content">
+      <div className="commentInput w-input-text" contentEditable="true" maxLength="1024">
       </div>
       );
   }
@@ -64,72 +72,15 @@ var CommentInput = React.createClass({
 var CommentForm = React.createClass({
   render: function() {
     return (
-      <form className="commentForm" >
-          <CommentInput />
-          <CommentSubmit onCommentSubmit={this.props.onCommentSubmit}/>
-      </form>
+      <div className="commentForm" >
+          <CommentInput ></CommentInput>
+          <CommentSubmit onCommentSubmit={this.props.onCommentSubmit}></CommentSubmit>
+      </div>
       );
   }
 });
 
 var CommentBox = React.createClass({
-    render: function() {
-        return (
-          <div className='commentBox'>
-              <CommentForm onCommentSubmit={this.handleCommentSubmit}/>
-              <CommentList data={this.props.data} />
-          </div>
-        );
-    }
-});
-
-var Article = React.createClass({
-  render: function() {
-      return (
-        <div className="ac">
-          <div className="article">
-            <h2 className='title'>
-              {this.props.data.title}
-            </h2>
-            <div className='text'>
-              {this.props.data.content}
-            </div>
-          </div>
-          <CommentBox  data={this.props.data.comment}/>
-        </div>
-        );
-  }
-});
-
-var ArticleList = React.createClass({
-  render: function() {
-    var articlesNodes = this.props.data.map(function(article){
-    return (
-            <Article key={article.id} data={article} />
-        );
-    });
-    return (
-      <div className='articleList'>
-        {articlesNodes}
-      </div>
-    );
-    }
-});
-
-var Blog = React.createClass({
-    loadCommentsFromServer: function() {
-      $.ajax({
-            url: this.props.url,
-            dataType: 'json',
-            cache: false,
-            success: function(data) {
-              this.setState({data: data});
-            }.bind(this),
-            error: function(xhr, status, err) {
-              console.error(this.props.url, status, err.toString());
-            }.bind(this)
-          });
-    },
     handleCommentSubmit: function(comment) {
       var comments = this.state.data;
       var newComments = comments.concat(comment);
@@ -148,23 +99,122 @@ var Blog = React.createClass({
       }.bind(this)
     });
     },
+    render: function() {
+        return (
+          <div className='commentBox'>
+              <CommentForm onCommentSubmit={this.handleCommentSubmit}></CommentForm>
+              <CommentList data={this.props.data}></CommentList>
+          </div>
+        );
+    }
+});
+
+var Article = React.createClass({
+  render: function() {
+      return (
+        <div className="ac">
+          <div className="article">
+            <h2 className='title'>
+              {this.props.data.title}
+            </h2>
+            <div className='text' dangerouslySetInnerHTML={rawMarkup(this.props.data.content)}>
+            </div>
+          </div>
+          <CommentBox data={this.props.data.comment}></CommentBox>
+        </div>
+        );
+  }
+});
+
+var ArticleList = React.createClass({
+  render: function() {
+    var articlesNodes = this.props.data.map(function(article){
+    return (
+            <Article key={article.id} data={article}></Article>
+        );
+    });
+    return (
+      <div className='articleList'>
+        {articlesNodes}
+      </div>
+    );
+    }
+});
+
+var limit = 3;
+var offset = 0;
+var remainder = limit + 1;
+var times = 0;
+var scroll_auto = -1;
+var Blog = React.createClass({
+    loadCommentsFromServer: function(url) {
+      $.ajax({
+            url: url,
+            dataType: 'json',
+            cache: false,
+            async:true,
+            success: function(data) {
+                this.setState({data: data});
+                window.onscroll = function () {
+                    //监听事件内容
+                }
+            }.bind(this),
+            error: function(xhr, status, err) {
+                console.error(this.props.url, status, err.toString());
+            }.bind(this)
+          });
+    },
+    test: function(url) {
+        blogs = this.state.data;
+        $.ajax({
+            url: url,
+            dataType: 'json',
+            cache: false,
+            async:true,
+            success: function(data) {
+                remainder = data.length;
+                this.setState({data: blogs.concat(data)});
+            }.bind(this),
+            error: function(xhr, status, err) {
+                console.error(this.props.url, status, err.toString());
+            }.bind(this)
+          });
+        return remainder
+    },
     getInitialState: function() {
+        $(window).mousewheel(function(event) {
+            url = this.props.url + '?limit=' + limit +'&offset={0}';
+            if (event.deltaY == -1) {
+                    if (remainder < limit) {
+                        $('#nothing').text('这次是真没了...');
+                    } else {
+                        if (scroll_auto != $(window)[0].scrollY) {
+                            scroll_auto = $(window)[0].scrollY;
+                        } else if (scroll_auto == $(window)[0].scrollY) {
+                            offset = limit * ++times;
+                            remainder = this.test(url.format(offset));
+                        }
+                    }
+            }
+        }.bind(this));
         return {data: []};
     },
     componentDidMount: function() {
-        this.loadCommentsFromServer();
+        url = this.props.url + '?limit=' + limit +'&offset={0}';
+        this.loadCommentsFromServer(url.format(offset));
         // setInterval(this.loadCommentsFromServer, this.props.pollInterval);
     },
   render: function() {
     return (
       <div className='blog'>
-        <ArticleList data={this.state.data}/>
+        <ArticleList data={this.state.data}></ArticleList>
+        <div id="nothing"></div>
       </div>
       );
   }
 });
 ReactDOM.render(
-  <Blog url='http://blog.cg.com/api/article' pollInterval={60000}/>,
+  <Blog url='http://blog.cg.com/api/v1/user/1/article' ></Blog>,
   document.getElementById('blogs')
 );
 
